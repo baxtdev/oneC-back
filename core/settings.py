0233d3bc8a9042582fff3dff11e6db3c7908f332
xshxt <habibulaevhushbaht05@gmail.com>
2023-12-05 17:58:37 +0600

    second bug

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,7 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_root")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static", "static_dirs"),
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- core/urls.py
+++ core/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+import debug_toolbar
 from rest_framework import routers
 from django.contrib import admin
 from django.urls import path
 from users.views import*
+from django.conf import settings
+from django.conf.urls.static import static
 
 router=routers.DefaultRouter()
 
@@ -30,3 +32,9 @@ urlpatterns = [
     path('api/v1/',include(router.urls)),
     path('api/v1/userGET/<int:user_id>',UserInfoGETapiVew.as_view(),name='Get info User')
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls)), ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
