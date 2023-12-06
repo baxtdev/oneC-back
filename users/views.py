b5d604054dd3a798b668d7584844a1defc085593
xshxt <habibulaevhushbaht05@gmail.com>
2023-12-06 15:34:18 +0600

    fixed bug 1

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.urls import path, include
 from django.contrib.auth.models import User
 
-from rest_framework import routers, serializers, viewsets,permissions,generics
+from rest_framework import routers, serializers, viewsets,permissions,generics,views
 from rest_framework.response import Response
 
 from .models import UserInfo
@@ -18,6 +18,12 @@ class UserInfoAPIViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         qs=[]
         return qs 
+    
+    def perform_create(self, serializer):
+        serializer.save()
+    
+    
+
 
 
 class UserInfoGETapiVew(generics.GenericAPIView):
@@ -30,3 +36,16 @@ class UserInfoGETapiVew(generics.GenericAPIView):
         user_info_instance = self.get_object()
         serializer = self.serializer_class(user_info_instance)
         return Response(serializer.data)  
+
+
+
+class ProcessDataAPIView(views.APIView):
+    permission_classes = [permissions.AllowAny]
+    def post(self, request, *args, **kwargs):
+        data_array = request.data
+        for data in data_array:
+            serializer = UserInfoSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+        
+        return Response({'message': 'Данные успешно сохранены'})
