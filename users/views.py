ee4df00ad539a871cfc0b9fe8f17a81f63398d13
xshxt <habibulaevhushbaht05@gmail.com>
2023-12-07 11:35:08 +0600

    add errors

--- users/views.py
+++ users/views.py
@@ -43,9 +43,21 @@ class ProcessDataAPIView(views.APIView):
     permission_classes = [permissions.AllowAny]
     def post(self, request, *args, **kwargs):
         data_array = request.data
-        for data in data_array:
-            serializer = UserInfoSerializer(data=data)
-            if serializer.is_valid():
-                serializer.save()
-        
-        return Response({'message': 'Данные успешно сохранены'})
+        if data_array:
+            try:
+                for data in data_array:
+                    serializer = UserInfoSerializer(data=data)
+                    try:
+                        if serializer.is_valid():
+                            serializer.save()
+                            return Response({'objects': f'{data_array}'},201)
+                        else:
+                            return Response ({"Error detail":"This object is don't created"},400)
+                    except Exception as e:
+                        print(f"Error detail: {e}")
+                        return Response ({"Error detail":f"{e}"},400)
+
+            except Exception as error: 
+                return Response({"Error detail":f"{error}"},400)
+        else:
+            return Response({"Error detail":"request array is null"},400)
